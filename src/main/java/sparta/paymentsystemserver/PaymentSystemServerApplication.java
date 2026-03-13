f441f8c09743ff955c3cd41c8be7eb297ba3be5c
jasmin <cjm98@smartel.co.kr>
2026-03-13 12:43:00 +0900

    feat: Initial commit

---
+++ src/main/java/sparta/paymentsystemserver/PaymentSystemServerApplication.java
@@ -0,0 +1,13 @@
+package sparta.paymentsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/paymentsystemserver/PaymentSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.paymentsystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
