e1967a2d769f8763ed47baf1ef6a6dfbee32dedf
jasmin <cjm98@smartel.co.kr>
2026-03-20 00:54:05 +0900

    fix : 유저 조회 반환 DTO 규격화

--- src/main/java/sparta/paymentsystemserver/domain/user/dto/UserResponse.java
+++ src/main/java/sparta/paymentsystemserver/domain/user/dto/UserResponse.java
@@ -1,4 +1,4 @@
 package sparta.paymentsystemserver.domain.user.dto;
 
-public record UserResponse(String customerUid,String email, String name, String phone,Long balance) {
+public record UserResponse(String customerUid,String email, String name, String phone,Long pointBalance) {
 }
