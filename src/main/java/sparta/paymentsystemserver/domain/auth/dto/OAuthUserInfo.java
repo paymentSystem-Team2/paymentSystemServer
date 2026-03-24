3077e9f7dbe87d92bb009a301ee5eb4b470815b4
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-03-24 15:11:49 +0900

    feat: Kakao OAuth2 적용 (#103)
    * feat: Google-OAuth2 구현
    
    * feat: Google-OAuth2 구현
    
    * feat: Kakao OAuth2 êµ적용

--- src/main/java/sparta/paymentsystemserver/domain/auth/dto/OAuthUserInfo.java
+++ src/main/java/sparta/paymentsystemserver/domain/auth/dto/OAuthUserInfo.java
@@ -27,4 +27,37 @@ public record OAuthUserInfo(
                 (String) attributes.get("sub")
         );
     }
+
+
+    /**
+     * 카카오가 보내주는 JSON 데이터
+     * {
+     *   "id"          // 카카오 고유 사용자 ID
+     *   "kakao_account": {
+     *     "email"
+     *     "profile": {
+     *       "nickname"
+     *     }
+     *   }
+     * }
+     */
+    public static OAuthUserInfo of(String registrationId, Map<String, Object> attributes) {
+        if ("kakao".equals(registrationId)) {
+            return fromKakao(attributes);
+        }
+        return from(attributes);
+    }
+
+    private static OAuthUserInfo fromKakao(Map<String, Object> attributes) {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        return new OAuthUserInfo(
+                (String) kakaoAccount.get("email"),
+                (String) profile.get("nickname"),
+                String.valueOf(attributes.get("id"))
+        );
+
+    }
 }
--- src/main/java/sparta/paymentsystemserver/domain/user/entity/AuthProvider.java
+++ src/main/java/sparta/paymentsystemserver/domain/user/entity/AuthProvider.java
@@ -2,5 +2,6 @@ package sparta.paymentsystemserver.domain.user.entity;
 
 public enum AuthProvider {
     LOCAL,
-    GOOGLE
+    GOOGLE,
+    KAKAO
 }
