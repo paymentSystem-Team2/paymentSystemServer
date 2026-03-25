dcb6010c200797fd61ac367bce66b3e8da6bdfdc
eunjiom <norgg021@gmail.com>
2026-03-25 10:17:33 +0900

    feat: 상품 이미지 추가 기능 구현 수정

--- src/main/java/sparta/paymentsystemserver/global/config/S3Config.java
+++ src/main/java/sparta/paymentsystemserver/global/config/S3Config.java
@@ -14,26 +14,8 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Value("${cloud.aws.credentials.access-key:}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key:}")
-    private String secretKey;
-
     @Bean
     public AmazonS3 amazonS3() {
-        // access key를 yml/.env에 넣은 경우
-        if (accessKey != null && !accessKey.isBlank()
-                && secretKey != null && !secretKey.isBlank()) {
-            BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-
-            return AmazonS3ClientBuilder.standard()
-                    .withRegion(region)
-                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                    .build();
-        }
-
-        // EC2 IAM Role 사용하는 경우
         return AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
