106826117cd2828ecae911327879915ae40af039
eunjiom <norgg021@gmail.com>
2026-03-25 10:24:13 +0900

    feat: 상품 이미지 추가 기능 구현 (#111)
    * feat: 주문 목록 조회 기능 구현
    
    * feat: 주문 목록 엔드포인트, 메서드 작성
    
    * feat: 주문 목록 조회 수정
    
    * feat: 주문 목록 조회 코드리뷰 수정
    
    * feat: 주문 목록 메서드명 변경
    
    * feat: 상품 상세 조회 기능 구현
    
    * feat: 상품 이미지 추가 기능 구현
    
    * feat: 상품 이미지 추가 기능 구현 수정

--- src/main/java/sparta/paymentsystemserver/domain/product/dto/GetProductDetailResponse.java
+++ src/main/java/sparta/paymentsystemserver/domain/product/dto/GetProductDetailResponse.java
@@ -1,11 +1,14 @@
 package sparta.paymentsystemserver.domain.product.dto;
 
+import java.util.List;
+
 public record GetProductDetailResponse(
         String id,
         String name,
         Long price,
         Long stock,
         String description,
-        String status
+        String status,
+        List<String> productImages
 ) {
 }
---
+++ src/main/java/sparta/paymentsystemserver/global/config/S3Config.java
@@ -0,0 +1,24 @@
+package sparta.paymentsystemserver.global.config;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                .build();
+    }
+}
